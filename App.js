fbbca6569e960030a7315777f9002981018576bc
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-09-18 22:46:45 -0300

    header component

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Header from './src/components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Word!</Text>
+    <View>
+      <Header title="People"/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 25,
+    backgroundColor: '#7aa9f5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 50,
+    color: '#fff',
+  }
+});
+
+export default styles;
