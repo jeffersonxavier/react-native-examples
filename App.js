a61475dc4421e17b5ade903f8e8cb0dc7d45bd48
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-09-19 22:15:33 -0300

    request ramdomuser.me

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import Header from './src/components/Header';
 
-export default function App() {
-  return (
-    <View>
-      <Header title="People"/>
-    </View>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { people: [] };
+  }
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api?results=5&nat=BR&exc=login,dob,registered,info,location,picture,id')
+      .then(res => res.json())
+      .then(({ results }) => this.setState({ people: results }));
+  }
+  
+  render() {
+    return (
+      <View>
+        <Header title="People"/>
+        { this.state.people.map(person => <Text key={person.name.first}>{ person.name.first } </Text>) }
+      </View>
+    );
+  }
 }
