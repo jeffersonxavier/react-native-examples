cf92d6c5429dba51a1ad2e2b0e2bb0c38456829a
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-10-26 15:12:24 -0300

    fixing login page layout

--- series/src/screens/Login.js
+++ series/src/screens/Login.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Text, Button, StyleSheet } from 'react-native';
 import FormInput from '../components/FormInput';
 
 const Login = () => {
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
+
+  const handleSubmit = () => {
+    console.log('login...', email, password);
+  }
 
   return (
     <View style={styles.container}>
@@ -19,6 +23,10 @@ const Login = () => {
         capitalize="none"
         secureText={true}
         setInput={setPassword}/>
+
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -27,6 +35,21 @@ const styles = StyleSheet.create({
   container: {
     padding: 15,
   },
+
+  button: {
+    marginTop: 40,
+    height: 42,
+    backgroundColor: '#6ca2f7',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 18,
+  }
 });
 
 export default Login;
