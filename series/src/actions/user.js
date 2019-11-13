923a53d2b557700aca1eaad7a9e1eb68e225789d
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-11-12 22:01:57 -0300

    define user actions

---
+++ series/src/actions/user.js
@@ -0,0 +1,10 @@
+export const USER_LOGIN = 'USER_LOGIN';
+export const userLogin = user => (
+  {
+    type: USER_LOGIN,
+    user,
+  }
+)
+
+export const USER_LOGOUT = 'USER_LOGOUT';
+export const userLogout = () => ({ type: USER_LOGOUT });
--- series/src/reducers/user.js
+++ series/src/reducers/user.js
@@ -1,5 +1,11 @@
-export default userReducer = (state = {}, action) => {
+import { USER_LOGOUT, USER_LOGIN } from '../actions';
+
+export default userReducer = (state = null, action) => {
   switch (action.type) {
+    case USER_LOGIN:
+      return action.user;
+    case USER_LOGOUT:
+      return null;
     default:
       return state;
   }
