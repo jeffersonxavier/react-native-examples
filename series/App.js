6eadfd7d7b30b89657ad88f33ca2bc25013e3522
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-11-12 21:36:53 -0300

    adding remote-redux-devtools

--- series/App.js
+++ series/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import devTools from 'remote-redux-devtools';
 import rootReducer from './src/reducers';
 import Routes from './src/Routes';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, devTools());
 
 export default function App() {
   return (
